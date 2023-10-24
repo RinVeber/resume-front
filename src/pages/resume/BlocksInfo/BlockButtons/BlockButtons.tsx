f1a51c9acede90aea94f687b0567eb08c1ff945d
Kirill Karpov <rin.veber@gmail.com>
2023-10-24 10:18:37 +0400

    вынес кнопки в компоненты

--- src/pages/resume/BlocksInfo/BlockButtons/BlockButtons.tsx
+++ src/pages/resume/BlocksInfo/BlockButtons/BlockButtons.tsx
@@ -1,57 +1,29 @@
-import { Stack, Button, Typography } from "@mui/material"
+import { Stack, Button, Typography } from '@mui/material';
 import del from '../../../../assets/delete.svg';
+import {
+  MyOutlinedButton,
+  MyDefaultButton,
+} from '../../../../components/Buttons';
 
 export default function BlockButtons() {
   return (
     <Stack mt={'59px'} flexDirection={'column'} gap={'36px'}>
-    <Stack flexDirection={'row'} gap={'32px'} justifyContent={'center'}>
+      <Stack flexDirection={'row'} gap={'32px'} justifyContent={'center'}>
+        <MyDefaultButton>Пригласить</MyDefaultButton>
+        <MyOutlinedButton>В избранное</MyOutlinedButton>
+      </Stack>
       <Button
         sx={{
-          fontSize: '16px',
-          '&:hover': { bgcolor: '#1D6BF3' },
-          bgcolor: '#5A9BFF',
-          color: '#FFFFFF',
-          padding: '15px 81px',
-        }}
-      >
-        Пригласить
-      </Button>
-      <Button
-        sx={{
-          fontSize: '16px',
-          '&:focus': {
-            bgcolor: '#1D6BF3',
-            border: '1px solid transparent',
-            color: '#FFFFFF',
-          },
+          fontSize: '14px',
           '&:hover': {
-            bgcolor: '#1D6BF3',
-            border: '1px solid transparent',
-            color: '#FFFFFF',
+            borderRadius: '16px',
+            backgroundColor: 'rgba(29, 107, 243, 0.11)',
           },
-
-          padding: '15px 81px',
-          color: '#1D6BF3',
-          border: '1px solid #1D6BF3',
         }}
       >
-        В избранное
+        <img src={del} alt="delete" />
+        <Typography sx={{ marginLeft: '4px' }}>Отказать кандидату</Typography>
       </Button>
     </Stack>
-    <Button
-      sx={{
-        fontSize: '14px',
-        '&:hover': {
-          borderRadius: '16px',
-          backgroundColor: 'rgba(29, 107, 243, 0.11)',
-        },
-      }}
-    >
-      <img src={del} alt="delete" />
-      <Typography sx={{ marginLeft: '4px' }}>
-        Отказать кандидату
-      </Typography>
-    </Button>
-  </Stack>
-  )
+  );
 }
