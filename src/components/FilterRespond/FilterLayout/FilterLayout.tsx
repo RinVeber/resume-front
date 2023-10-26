0008a27a2eb6b9e51ecd65041fba9bcaadce52c9
Kirill Karpov <rin.veber@gmail.com>
2023-10-26 20:18:14 +0400

    fix button in filter layout

--- src/components/FilterRespond/FilterLayout/FilterLayout.tsx
+++ src/components/FilterRespond/FilterLayout/FilterLayout.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import { Checkbox, Box, Autocomplete, TextField, InputAdornment } from '@mui/material';
+import {
+  Checkbox,
+  Autocomplete,
+  TextField,
+  InputAdornment,
+} from '@mui/material';
 import del from '../../../assets/delete.svg';
 import SearchIcon from '@mui/icons-material/Search';
+import {ListItemButtonCustom} from '../CustomListItemButton/CustomListItemButton'
 
 export default function FilterLayout() {
   const [openStatus, setOpenStatus] = React.useState(false);
@@ -52,39 +57,37 @@ export default function FilterLayout() {
     <List
       sx={{ width: '100%', bgcolor: 'background.paper' }}
       component="nav"
-      aria-labelledby="nested-list-subheader"
     >
-      <ListItemButton onClick={handleClickStatus}>
+      <ListItemButtonCustom  onClick={handleClickStatus}>
         <ListItemText primary="Статус" />
         {openStatus ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
+      </ListItemButtonCustom>
       <Collapse in={openStatus} timeout="auto" unmountOnExit>
-        <List sx={{ padding: '4px' }} component="div" disablePadding>
-          <ListItemButton>
+        <List component="div" disablePadding>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Откликнулись" />
-          </ListItemButton>
-          <ListItemButton>
+          </ListItemButtonCustom>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Без отклика" />
-          </ListItemButton>
+          </ListItemButtonCustom>
         </List>
       </Collapse>
 
-      <ListItemButton onClick={handleClickCity}>
+      <ListItemButtonCustom onClick={handleClickCity}>
         <ListItemText primary="Город" />
         {openСity ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
+      </ListItemButtonCustom>
       <Collapse in={openСity} timeout="auto" unmountOnExit>
-        <List sx={{ padding: '4px' }} component="div" disablePadding>
-          <ListItemButton>
+        <List component="div" disablePadding>
+          <ListItemButtonCustom>
             <Autocomplete
               disableCloseOnSelect
               filterSelectedOptions
               disablePortal
-      
               options={listCityMok}
               getOptionLabel={(option) => option.name}
               sx={{ width: '330px' }}
@@ -92,7 +95,6 @@ export default function FilterLayout() {
               renderInput={(params) => (
                 <TextField
                   {...params}
-            
                   fullWidth
                   InputProps={{
                     startAdornment: (
@@ -105,74 +107,82 @@ export default function FilterLayout() {
                 />
               )}
             />
-          </ListItemButton>
+          </ListItemButtonCustom>
         </List>
       </Collapse>
 
-      <ListItemButton onClick={handleClickSalary}>
+      <ListItemButtonCustom onClick={handleClickSalary}>
         <ListItemText primary="Зарплата" />
         {openSalary ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
+      </ListItemButtonCustom>
       <Collapse in={openSalary} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <Box
-            sx={{ display: 'flex', flexDirection: 'row', gap: '5px', pl: 4 }}
-          >
-            <TextField placeholder="От" fullWidth />
-            <TextField placeholder="До" fullWidth />
-          </Box>
+        <List component="div" disablePadding sx={{ display: 'flex', flexDirection: 'row', gap: '5px', justifyContent: 'center' }}>
+       
+            <TextField sx={{
+                width: '120px',
+                '& input': {
+                    padding: '9px 5px'
+                }
+            }} placeholder="От"  />
+            <TextField sx={{
+                width: '120px',
+                '& input': {
+                    padding: '9px 5px'
+                }
+            }} placeholder="До"  />
+   
         </List>
       </Collapse>
 
-      <ListItemButton onClick={handleClickFormat}>
+      <ListItemButtonCustom onClick={handleClickFormat}>
         <ListItemText primary="Форматы работы" />
         {openFormat ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
+      </ListItemButtonCustom>
       <Collapse in={openFormat} timeout="auto" unmountOnExit>
-        <List sx={{ padding: '4px' }} component="div" disablePadding>
-          <ListItemButton>
+        <List component="div" disablePadding>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Офис" />
-          </ListItemButton>
-          <ListItemButton>
+          </ListItemButtonCustom>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Гибрид" />
-          </ListItemButton>
-          <ListItemButton>
+          </ListItemButtonCustom>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Удаленка" />
-          </ListItemButton>
+          </ListItemButtonCustom>
         </List>
       </Collapse>
 
-      <ListItemButton onClick={handleClickPortfolio}>
+      <ListItemButtonCustom onClick={handleClickPortfolio}>
         <ListItemText primary="Наличие портфолио" />
         {openPortfolio ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
+      </ListItemButtonCustom>
       <Collapse in={openPortfolio} timeout="auto" unmountOnExit>
-        <List sx={{ padding: '4px' }} component="div" disablePadding>
-          <ListItemButton>
+        <List component="div" disablePadding>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Есть портфолио" />
-          </ListItemButton>
-          <ListItemButton>
+          </ListItemButtonCustom>
+          <ListItemButtonCustom>
             <Checkbox />
 
             <ListItemText primary="Есть сопроводительное письмо" />
-          </ListItemButton>
+          </ListItemButtonCustom>
         </List>
       </Collapse>
 
-      <ListItemButton
+      <ListItemButtonCustom
         sx={{ display: 'flex', flexDirection: 'row', gap: '5px' }}
       >
         <img src={del} alt={'delete'} />
         <ListItemText primary="Отчистить фильтры" />
-      </ListItemButton>
+      </ListItemButtonCustom>
     </List>
   );
 }
