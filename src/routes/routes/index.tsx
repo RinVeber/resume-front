75c452b001dc3f8918ae34cd46743d4fb184e7c8
Anton Lyscov <anton.lyscov@yandex.ru>
2023-10-26 14:28:59 +0300

    отредактировал структуру

--- src/routes/routes/index.tsx
+++ src/routes/routes/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { paths } from './paths';
 import NotFound from '../../pages/not-found/NotFound';
-import Resume from '../../pages/resume/Resume';
+// import Resume from '../../pages/resume/Resume';
 import CreateVacancies from '../../pages/create-vacancies/CreateVacancies';
-import Vacancies from '../../pages/vacancies/Vacancies';
+// import Vacancies from '../../pages/vacancies/Vacancies';
 import VacanciesLayout from '../VacanciesLayout';
-// import MainPage from '../../pages/main-page/MainPage';
-import Respond from '../../pages/respond/Respond';
+import MainPage from '../../pages/main-page/MainPage';
+// import Respond from '../../pages/respond/Respond';
 import RootLayout from '../RootLayout';
 import Auth from '../../pages/auth/Auth';
 import AuthLayout from '../AuthLayout';
@@ -28,31 +28,21 @@ export const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {
-        path: paths.notFound,
-        element: <NotFound />,
+        path: '/',
+        element: <MainPage />
       },
       {
-        path: paths.main,
+        path: paths.vacancies,
         element: <VacanciesLayout />,
-        children: [
-          {
-            path: paths.vacancies,
-            element: <Vacancies />,
-          }
-        ]
-      },
-      {
-        path: paths.respond,
-        element: <Respond />,
-      },
-      {
-        path: `${paths.resume}/:id`,
-        element: <Resume />,
       },
       {
         path: paths.create,
         element: <CreateVacancies />,
       },
     ],
+  },
+  {
+    path: paths.notFound,
+    element: <NotFound />,
   },
 ]);
