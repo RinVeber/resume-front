ee0061fbea5e0d4e04b836e891baa57d6d01c31a
Kirill Karpov <rin.veber@gmail.com>
2023-10-27 19:58:24 +0400

    fix checkbox

--- src/pages/auth/InputsField/InputField.tsx
+++ src/pages/auth/InputsField/InputField.tsx
@@ -1,8 +1,9 @@
-import { Typography, Stack, Checkbox, Box, Button } from '@mui/material';
+import { Typography, Stack, Box, Button } from '@mui/material';
 import { useFormContext } from 'react-hook-form';
 import { FormAuth } from '../libs/ValidSchema';
 import InputLayout from './InputLayout/InputLayout';
 import { SlideForgot } from '../../../components/SlideBar';
+import CustomCheckbox from '../../../components/Checkbox';
 
 export default function InputField() {
   const {
@@ -22,15 +23,8 @@ export default function InputField() {
         alignItems={'center'}
       >
         <Box display={'flex'} flexDirection={'row'} alignItems={'center'}>
-          <Checkbox
-            sx={{
-              color: '#1D6BF3',
-              '&.Mui-checked': {
-                color: '#1D6BF3',
-              },
-              '& .MuiSvgIcon-root': { fontSize: 24 },
-            }}
-          />
+          <CustomCheckbox />
+    
           <Typography variant="h6" sx={{ color: '#797981' }}>
             Запомнить
           </Typography>
