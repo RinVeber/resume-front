938849ca23f93f098a0c930e881dc8023336baa8
Kirill Karpov <rin.veber@gmail.com>
2023-10-30 00:54:20 +0400

    resume page api

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,10 +1,20 @@
 import { Box } from '@mui/material';
-
 import HeaderResume from './HeaderResume/HeaderResume';
-
 import BlocksInfo from './BlocksInfo/BlocksInfo';
+import { getResume } from '../../redux/slice/resumeSlice';
+import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
 
 export default function Resume() {
+  const {id} = useParams();
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    dispatch(getResume(id))
+  }, [dispatch]);
+
+  
   return (
     <Box
       sx={{
--- src/utils/apiConstants.ts
+++ src/utils/apiConstants.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const API_BASE_URL = `http://career-tracker.duckdns.org/api/v1/`
+export const API_BASE_URL = `http://career-tracker.duckdns.org/api/v1`
 
 
 export const API_BASE_ALL_VACANCIES_URL = API_BASE_URL + '/vacancies';
