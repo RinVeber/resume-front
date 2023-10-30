c02a4c973c0f1e600ed94bc19bfabfa5f44e3ddb
Kirill Karpov <rin.veber@gmail.com>
2023-10-30 16:49:26 +0400

    fix lazy load

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,13 +1,13 @@
 import { Box } from '@mui/material';
 import { getResume } from '../../redux/slice/resumeSlice';
 import { useParams } from 'react-router-dom';
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
-import ErrorServer from '../../components/Error';
 import Loading from '../../components/Loading';
 
 const HeaderResume = lazy(() => import('./HeaderResume/HeaderResume'));
 const BlocksInfo = lazy(() => import('./BlocksInfo/BlocksInfo'));
+const ErrorServer = lazy(() => import('../../components/Error'))
 
 export default function Resume() {
   const { id } = useParams();
@@ -33,15 +33,18 @@ export default function Resume() {
           // maxWidth: '1190px',
           width: '100%',
           display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'center',
           gap: '40px',
           padding: '0 40px 0 0',
           backgroundColor: '#F9FAFB',
         }}
       >
+        <Suspense fallback={<Loading />}>
         {status == 'error' && <ErrorServer />}
         {status == 'success' && <HeaderResume />}
         {status == 'success' && <BlocksInfo />}
+        </Suspense>
       </Box>
     </>
   );
