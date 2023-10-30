a2420fe0ca61726c109c4458e973d8b41397f7d5
Kirill Karpov <rin.veber@gmail.com>
2023-10-30 22:22:36 +0400

    fix

--- src/utils/mokData.ts
+++ src/utils/mokData.ts
@@ -7,6 +7,183 @@ export const dataMokCardVacancies = [
     city: 'Москва',
     suiltcase: '2 года',
     avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Сергей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Жорик Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Жорик Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: false,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+];
+
+export const mokDataResume = {
+  name: 'Королев Василий',
+  city: 'Чебоксары',
+language: 'Английский А1',
+  experience: '2 Года',
+  format: 'удаленно',
+  incomeLevel: '70 000',
+skills: 'Figma, Photoshop',
+courses: 'Яндекс Практикум 2021',
+univer: 'Чебоксарский технический университет, 2009',
+
+};
+
+
+export const dataMokCardVacanciesFavorites = [
+  {
+    id: 1,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
     isCheked: true,
     skills: [
       {
@@ -79,17 +256,86 @@ export const dataMokCardVacancies = [
       },
     ],
   },
+  {
+    id: 6,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: true,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: true,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: true,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: 'Алексей Долматов',
+    subname: 'UI/UX Дизайнер',
+    coincidence: '100% совпадает вакансия',
+    city: 'Москва',
+    suiltcase: '2 года',
+    avatar: '',
+    isCheked: true,
+    skills: [
+      {
+        id: 1,
+        title: 'Figma',
+      },
+      {
+        id: 2,
+        title: 'Photoshop',
+      },
+    ],
+  },
 ];
 
-export const mokDataResume = {
-  name: 'Королев Василий',
-  city: 'Чебоксары',
-language: 'Английский А1',
-  experience: '2 Года',
-  format: 'удаленно',
-  incomeLevel: '70 000',
-skills: 'Figma, Photoshop',
-courses: 'Яндекс Практикум 2021',
-univer: 'Чебоксарский технический университет, 2009',
 
-};
